469331590545ed90ba8d9f0914a01072630af15a
Bianca Mazzi <bianca.mazzi@hotmail.com>
2020-08-12 12:40:23 -0300

    carrossel

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// CONTADOR
 let valorAtual = 0;
 const valorLimite = parseInt($(".counter h2").text());
 
@@ -15,3 +16,27 @@ function soma(){
 function pararSoma() {
     clearInterval(loopingSoma);
   }
+
+
+// CARROSSEL
+
+$(document).ready(function(){
+    $('#cards-thumbs').slick({
+        infinite: true,
+        dots: true,
+        arrows: false,
+        responsive: [
+            {
+              breakpoint: 5000,
+              settings: "unslick"
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+  });
